ce1da15ce0d8d20f3e315de27bf848943df38497
AsmShuvo <hoffmanhulk99@gmail.com>
2026-02-10 01:18:10 +0600

    add newFn to MyClass

--- B/SubB/MyClass.java
+++ B/SubB/MyClass.java
@@ -2,4 +2,8 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello from MyClass in SubB!");
     }
+
+    void newFn() {
+        System.out.println("This is a new function in MyClass.");
+    }
 }
